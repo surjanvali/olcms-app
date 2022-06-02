141821b18285e90b294a251eafee35aca57ad432
surjanvali <surjanvali@gmail.com>
2022-06-02 15:43:54 +0530

    API updation

--- src/in/apcfss/struts/eCourt/apis/ImportECourtsData.java
+++ src/in/apcfss/struts/eCourt/apis/ImportECourtsData.java
@@ -60,6 +60,7 @@ public class ImportECourtsData
 			// sql = "select distinct cino from ecourts_cinos_new where to_char(inserted_time,'dd/mm/yyyy')='03/02/2022' and ecourts_response is null and cino='APHC010000942022'";
 			
 			// sql = "select distinct a.cino from ecourts_contempt_cinos a left join ecourts_case_data b using (cino) where b.cino is null ";
+			
 			sql ="select distinct a.cino from ecourts_cinos_new a where cino='APHC010080012022'";
 			
 			// sql = "select distinct a.cino from ecourts_cinos_1105_1605 a left join ecourts_case_data b using (cino) where b.cino is null ";
