b2a4c28972ef1b1714e56fef07d7724d29e9f426
Surjan Vali <surjanvali@gmail.com>
2022-06-04 16:48:28 +0530

    updatedc1234

--- src/in/apcfss/struts/eCourts/schedulars/QuartzPlugin.java
+++ src/in/apcfss/struts/eCourts/schedulars/QuartzPlugin.java
@@ -24,21 +24,24 @@ public class QuartzPlugin implements PlugIn {
 	public void init(ActionServlet servlet, ModuleConfig config)
 			throws ServletException {
 
-		JobDetail job = JobBuilder.newJob(SchedulerJob.class)
-				.withIdentity("anyJobName", "group1").build();
+		// JobDetail job = JobBuilder.newJob(SchedulerJob.class).withIdentity("anyJobName", "group1").build();
+		JobDetail job = JobBuilder.newJob(HighCourtCasesDataSchedular.class).withIdentity("HighCourtCasesDataSchedular", "group2").build();
 
 		try {
 
-			Trigger trigger = TriggerBuilder
-					.newTrigger()
-					.withIdentity("anyTriggerName", "group1")
-					.withSchedule(
-							CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
-					.build();
+			Trigger trigger = TriggerBuilder.newTrigger().withIdentity("anyTriggerName", "group1").withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * *")).build();
 
 			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
 			scheduler.start();
 			scheduler.scheduleJob(job, trigger);
+			
+			
+			/*Trigger trigger2 = TriggerBuilder.newTrigger().withIdentity("anyTriggerName", "group2").withSchedule(CronScheduleBuilder.cronSchedule("0/1 * * * * ?")).build();
+
+			Scheduler scheduler2 = new StdSchedulerFactory().getScheduler();
+			scheduler2.start();
+			scheduler2.scheduleJob(job2, trigger2);
+			*/
 
 		} catch (Exception e) {
 			e.printStackTrace();
